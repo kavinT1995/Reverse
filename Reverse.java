cf499ee95bc7d0c589ad9fa76bd7ab052c877d1f
kavinT1995 <kavin.tcse@ksrce.ac.in>
2016-07-22 14:01:10 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+# Reverse
+public class Reverse {
+
+	
+	public static void main(String[] args) {
+		String str="java";
+		StringBuffer sr=new StringBuffer(str);
+		System.out.println(sr.reverse());
+	}
+
+}
